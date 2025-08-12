bb52be017c917ed64baa4b722b89afbdb9449941
AURELIEN2004 <feudjioaurelien24@gmail.com>
2025-08-12 12:16:03 +0100

    modif

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,76 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbar.scss'
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { IoCloseCircleSharp } from "react-icons/io5";
+import { TbGridDots } from "react-icons/tb";
+
 
 
 const Navbar = () => {
+
+  const [active, setActive] = useState('navBar')
+  const showNav = ()=>{
+    setActive('navBar activeNavbar ')
+  }
+
   return (
-    <div>
-      Navbar
-    </div>
+    <section className="navBarSection">
+      <div className="header">
+
+        <div className="logoDiv">
+          <a href="/" className="logo">
+            <h1 className='flex'><MdOutlineTravelExplore className='icon' />
+                 Dot
+             </h1>
+          </a>
+        </div>
+
+        <div className="(active)">
+          <ul className="navlists flex">
+
+            <li className="navItem">
+              <a href="/" className="navLink">Home</a>
+            </li>
+
+            <li className="navItem">
+              <a href="/" className="navLink">Products</a>
+            </li>
+
+            <li className="navItem">
+              <a href="/" className="navLink">Resources</a>
+            </li>
+
+            <li className="navItem">
+              <a href="/" className="navLink">Contact</a>
+            </li>
+
+            <li className="navItem">
+              <a href="/" className="navLink">Blog</a>
+            </li>
+
+            <div className="headerBtns flex"> 
+              <button className="btn loginBtn">
+                <a href="/" className="btnLink">Login</a>
+              </button>
+
+              <button className="btn">
+                <a href="/" className="btnLink">Sign Up</a>
+              </button>
+            </div>
+
+          </ul> 
+
+          <div className="closeNavbar">
+            <IoCloseCircleSharp className='icon'/>
+          </div>
+        </div>
+
+        <div onClick={showNav} className="toggleNavbar">
+          < TbGridDots className="icon"/>
+        </div>
+        
+      </div>
+    </section>
   )
 }
 
